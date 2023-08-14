8b7b5cb8bbaa753e0acb0633a4bcccad0a6c6f72
DaylightNebula <35437205+DaylightNebula@users.noreply.github.com>
2023-08-14 11:05:01 -0600

    Attempt to fix memory leak by removing the need to create a new thread and client every attempt

--- DaylinMicroservices-Core/src/main/kotlin/daylightnebula/daylinmicroservices/core/requests/Requester.kt
+++ DaylinMicroservices-Core/src/main/kotlin/daylightnebula/daylinmicroservices/core/requests/Requester.kt
@@ -11,40 +11,37 @@ import kotlinx.coroutines.runBlocking
 import org.json.JSONException
 import org.json.JSONObject
 import org.slf4j.Logger
+import java.awt.SystemColor.text
 import java.util.concurrent.CompletableFuture
 import kotlin.concurrent.thread
 
 internal object Requester {
-    fun rawRequest(logger: Logger, address: String, json: JSONObject): CompletableFuture<Result<JSONObject>> {
-        val future = CompletableFuture<Result<JSONObject>>()
+    private val client = HttpClient(CIO) {
+        install(HttpTimeout) {
+            requestTimeoutMillis = 5000
+        }
+    }
 
-        // start a thread to run the request async
-        thread {
-            runBlocking {
-                // send get request with timeout, if any error occurs, just return null
-                val response = try {
-                    HttpClient(CIO) {
-                        install(HttpTimeout) {
-                            requestTimeoutMillis = 5000
-                        }
-                    }.get(address, block = {
-                        parameter("json", json.toString(1))
-                    })
-                } catch (ex: Exception) { logger.warn("Request failed with exception: ${ex.message}"); null }
+    fun rawRequest(logger: Logger, address: String, json: JSONObject) = CompletableFuture.supplyAsync {
+        runBlocking {
+            // send get request with timeout, if any error occurs, just return null
+            val response = try {
+                client.get(address, block = {
+                    parameter("json", json.toString(1))
+                })
+            } catch (ex: Exception) {
+                logger.warn("Request failed with exception: ${ex.message}"); null
+            }
 
-                // when request completes, call the on complete function, using try catch in case json conversion fails
-                val text = response?.bodyAsText() ?: "{}"
-                val result = try {
-                    JSONObject(text).toResult()
-                } catch (ex: JSONException) {
-                    val error = "Invalid input json received: $text"
-                    logger.error(error)
-                    Result.Error(error)
-                }
-                future.complete(result)
+            // when request completes, call the on complete function, using try catch in case json conversion fails
+            val text = response?.bodyAsText() ?: "{\"error\":\"Response did not contain json\"}"
+            try {
+                JSONObject(text).toResult()
+            } catch (ex: JSONException) {
+                val error = "Invalid input json received: $text"
+                logger.error(error)
+                Result.Error(error)
             }
         }
-
-        return future
     }
 }
